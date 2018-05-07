c9ce7626c47af115d8e9239fbe0b34b1ae2d1185
mewmew <rnd0x00@gmail.com>
2018-05-07 20:01:53 +0200

    cmd/speak: determine start production automatically

--- cmd/speak/main.go
+++ cmd/speak/main.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"unicode"
 	"unicode/utf8"
 
 	"github.com/mewkiz/pkg/ioutilx"
@@ -40,15 +42,24 @@ func main() {
 		start string
 	)
 	flag.StringVar(&grammarPath, "g", "grammar.ebnf", "path to EBNF grammar")
-	flag.StringVar(&start, "start", "Start", "start production rule")
+	flag.StringVar(&start, "start", "", "start production rule")
 	flag.Usage = usage
 	flag.Parse()
 
-	// Parse grammar.
-	grammar, err := parseGrammar(grammarPath, start)
+	// Parse and validate grammar.
+	grammar, first, err := parseGrammar(grammarPath)
 	if err != nil {
 		log.Fatalf("%+v", err)
 	}
+	if len(start) == 0 {
+		start = first
+	}
+	dbg.Println("start:", start)
+	if err := ebnf.Verify(grammar, start); err != nil {
+		log.Fatalf("%+v", errors.WithStack(err))
+	}
+
+	// Parse input by runtime evaluation of the grammar.
 	for _, inputPath := range flag.Args() {
 		input, err := ioutilx.ReadFile(inputPath)
 		if err != nil {
@@ -81,51 +92,51 @@ type parser struct {
 	input []byte
 	// Current position in input source.
 	pos int
-	// TODO: Remove?
-	// Reached EOF.
+	// End of input has been reached.
 	eof bool
 }
 
 func (p *parser) evalProd(x *ebnf.Production) bool {
-	dbg.Print("evalProd:")
+	dbg.Println("evalProd:", exprString(x))
 	dbg.Printf("   name: %s", x.Name.String)
 	return p.evalExpr(x.Expr)
 }
 
 func (p *parser) evalExpr(x ebnf.Expression) bool {
+	dbg.Println("evalExpr:", exprString(x))
 	if p.eof {
 		return true
 	}
-	dbg.Print("evalExpr:")
 	switch x := x.(type) {
 	case *ebnf.Production:
 		panic(fmt.Errorf("support for expression %T not yet implemented", x))
 	case ebnf.Alternative:
 		return p.evalAlt(x)
+	case ebnf.Sequence:
+		return p.evalSeq(x)
 	case *ebnf.Name:
 		return p.evalName(x)
+	case *ebnf.Token:
+		return p.evalToken(x)
 	case *ebnf.Range:
 		return p.evalRange(x)
 	case *ebnf.Group:
 		panic(fmt.Errorf("support for expression %T not yet implemented", x))
 	case *ebnf.Option:
-		panic(fmt.Errorf("support for expression %T not yet implemented", x))
+		return p.evalOpt(x)
 	case *ebnf.Repetition:
 		return p.evalRep(x)
-	case ebnf.Sequence:
-		return p.evalSeq(x)
-	case *ebnf.Token:
-		panic(fmt.Errorf("support for expression %T not yet implemented", x))
 	default:
 		panic(fmt.Errorf("support for expression %T not yet implemented", x))
 	}
 }
 
-func (p *parser) evalAlt(alt ebnf.Alternative) bool {
-	for _, x := range alt {
+func (p *parser) evalAlt(x ebnf.Alternative) bool {
+	dbg.Println("evalAlt:", exprString(x))
+	for _, e := range x {
 		// record pos, and reset if alternative mismatches.
 		bak := p.pos
-		if p.evalExpr(x) {
+		if p.evalExpr(e) {
 			return true
 		}
 		// reset pos.
@@ -134,42 +145,44 @@ func (p *parser) evalAlt(alt ebnf.Alternative) bool {
 	return false
 }
 
-func (p *parser) evalSeq(seq ebnf.Sequence) bool {
-	for _, x := range seq {
-		if !p.evalExpr(x) {
+func (p *parser) evalSeq(x ebnf.Sequence) bool {
+	dbg.Println("evalSeq:", exprString(x))
+	for _, e := range x {
+		if !p.evalExpr(e) {
 			return false
 		}
 	}
 	return true
 }
 
-func (p *parser) evalRep(x *ebnf.Repetition) bool {
-	dbg.Print("evalRep:")
-	if p.eof {
-		return true
-	}
-	for p.evalExpr(x.Body) {
-		if p.eof {
-			return true
+func (p *parser) evalName(x *ebnf.Name) bool {
+	dbg.Println("evalName:", exprString(x))
+	prod := p.grammar[x.String]
+	return p.evalProd(prod)
+}
+
+func (p *parser) evalToken(x *ebnf.Token) bool {
+	dbg.Println("evalToken:", exprString(x))
+	for _, q := range x.String {
+		r := p.nextRune()
+		if r == eof {
+			return false
+		}
+		if r != q {
+			return false
 		}
 	}
 	return true
 }
 
-func (p *parser) evalName(x *ebnf.Name) bool {
-	prod := p.grammar[x.String]
-	return p.evalProd(prod)
-}
-
 func (p *parser) evalRange(x *ebnf.Range) bool {
+	dbg.Println("evalRange:", exprString(x))
 	f, _ := utf8.DecodeRuneInString(x.Begin.String)
 	t, _ := utf8.DecodeRuneInString(x.End.String)
 	r := p.nextRune()
-	dbg.Println("evalRange:")
 	if r == eof {
-		p.eof = true
 		dbg.Println("   eof")
-		return true
+		return false
 	}
 	ret := f <= r && r <= t
 	dbg.Printf("   r: %c", r)
@@ -179,7 +192,68 @@ func (p *parser) evalRange(x *ebnf.Range) bool {
 	return ret
 }
 
+func (p *parser) evalOpt(x *ebnf.Option) bool {
+	dbg.Println("evalOpt:", exprString(x))
+	if p.eof {
+		return true
+	}
+	// store position and try to parse optional.
+	bak := p.pos
+	if !p.evalExpr(x.Body) {
+		// reset position
+		p.pos = bak
+	}
+	return true
+}
+
+func (p *parser) evalRep(x *ebnf.Repetition) bool {
+	dbg.Println("evalRep:", exprString(x))
+	for !p.eof && p.evalExpr(x.Body) {
+	}
+	return true
+}
+
 // ### [ Helper functions ] ####################################################
+
+// exprString returns the string representation of the given EBNF expression.
+func exprString(x ebnf.Expression) string {
+	switch x := x.(type) {
+	case *ebnf.Production:
+		return fmt.Sprintf("%v = %v .", exprString(x.Name), exprString(x.Expr))
+	case ebnf.Alternative:
+		buf := strings.Builder{}
+		for i, e := range x {
+			if i != 0 {
+				buf.WriteString(" | ")
+			}
+			buf.WriteString(exprString(e))
+		}
+		return buf.String()
+	case ebnf.Sequence:
+		buf := strings.Builder{}
+		for i, e := range x {
+			if i != 0 {
+				buf.WriteString(" ")
+			}
+			buf.WriteString(exprString(e))
+		}
+		return buf.String()
+	case *ebnf.Name:
+		return x.String
+	case *ebnf.Token:
+		return fmt.Sprintf("%q", x.String)
+	case *ebnf.Range:
+		return fmt.Sprintf("%v … %v", exprString(x.Begin), exprString(x.End))
+	case *ebnf.Group:
+		panic(fmt.Errorf("support for expression %T not yet implemented", x))
+	case *ebnf.Option:
+		return fmt.Sprintf("[ %v ]", exprString(x.Body))
+	case *ebnf.Repetition:
+		return fmt.Sprintf("{ %v }", exprString(x.Body))
+	default:
+		panic(fmt.Errorf("support for expression %T not yet implemented", x))
+	}
+}
 
 // eof signals end of input.
 const eof rune = -1
@@ -187,6 +261,7 @@ const eof rune = -1
 // nextRune returns the next Unicode rune of the input source.
 func (p *parser) nextRune() rune {
 	if p.pos >= len(p.input) {
+		p.eof = true
 		return eof
 	}
 	r, size := utf8.DecodeRune(p.input[p.pos:])
@@ -194,21 +269,34 @@ func (p *parser) nextRune() rune {
 	return r
 }
 
-// parseGrammar parses the given EBNF grammar and verifies it from the start
+// parseGrammar parses the given EBNF grammar and determines its start
 // production rule.
-func parseGrammar(grammarPath, start string) (ebnf.Grammar, error) {
+func parseGrammar(grammarPath string) (ebnf.Grammar, string, error) {
 	f, err := os.Open(grammarPath)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, "", errors.WithStack(err)
 	}
 	defer f.Close()
 	br := bufio.NewReader(f)
 	grammar, err := ebnf.Parse(grammarPath, br)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, "", errors.WithStack(err)
 	}
-	if err := ebnf.Verify(grammar, start); err != nil {
-		return nil, errors.WithStack(err)
+	// Find first syntactic production rule by minimum file offset.
+	var first string
+	min := -1
+	for name, prod := range grammar {
+		r, _ := utf8.DecodeRuneInString(name)
+		if unicode.IsUpper(r) {
+			off := prod.Name.Pos().Offset
+			if min == -1 || off < min {
+				first = name
+				min = off
+			}
+		}
 	}
-	return grammar, nil
+	if len(first) == 0 {
+		return nil, "", errors.Errorf("unable to located first syntactic production rule (capital letter) in grammar %q", grammarPath)
+	}
+	return grammar, first, nil
 }
